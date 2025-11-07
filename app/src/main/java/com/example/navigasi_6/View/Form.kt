211bc27a8add7db26e3220d9d31e10f4060d9439
ZakyMalika <zakymalika0987@gmail.com>
2025-11-07 14:59:56 +0700

    membuat function

--- app/src/main/java/com/example/navigasi_6/View/Form.kt
+++ app/src/main/java/com/example/navigasi_6/View/Form.kt
@@ -1,3 +1,15 @@
 package com.example.navigasi_6.View
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormIsian(
+    jenisK:List<String> = Listof("Laki-laki","Perempuan"),
+    OnSubmitBtnClick:()-> Unit
+){
+    Scaffold(modifier = Modifier) {  }
+}
