9c3f7da75b0a7aa35ff73076a96ad8475e6cc74a
ZakyMalika <zakymalika0987@gmail.com>
2025-11-07 15:50:48 +0700

    membuat variabel tetap untuk data static

--- app/src/main/java/com/example/navigasi_6/Navigasi.kt
+++ app/src/main/java/com/example/navigasi_6/Navigasi.kt
@@ -2,6 +2,7 @@ package com.example.navigasi_6
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 import java.lang.reflect.Modifier
 
 
@@ -11,6 +12,8 @@ fun TampilData(
     onBackBtnClick:()-> Unit
 ){
     val items = listOf(
-        Pair
+        Pair(stringResource(id = R.string.nama_lengkap),"Contoh Nama"),
+        Pair(stringResource(id = R.string.jenis_kelamin),"Lainnya"),
+        Pair(stringResource(id = R.string.alamat),"Bengkulu"),
     )
 }
