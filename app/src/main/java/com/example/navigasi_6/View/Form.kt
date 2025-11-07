d3df9de11e4eea876424cd4f64eabffbb8d65040
ZakyMalika <zakymalika0987@gmail.com>
2025-11-07 15:12:27 +0700

    membuat Column untuk isi ruang

--- app/src/main/java/com/example/navigasi_6/View/Form.kt
+++ app/src/main/java/com/example/navigasi_6/View/Form.kt
@@ -1,11 +1,16 @@
 package com.example.navigasi_6.View
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
@@ -19,13 +24,23 @@ fun FormIsian(
     jenisK:List<String> = listOf("Laki-laki","Perempuan"),
     OnSubmitBtnClick:()-> Unit
 ){
-    Scaffold(modifier = Modifier),
+    Scaffold(modifier = Modifier,
             {
         TopAppBar(
             title = { Text(stringResource(id = R.string.home), color = Color.White) },
             colors = TopAppBarDefaults.mediumTopAppBarColors(
                 colorResource(id = R.color.teal_200))
-        )
-    }
+        )}
+    ){ isiRuang ->
+        Column(modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            OutlinedTextField(
+                value = "",
+                singleLine = true
 
+            )
+        }
+
+    }
 }
