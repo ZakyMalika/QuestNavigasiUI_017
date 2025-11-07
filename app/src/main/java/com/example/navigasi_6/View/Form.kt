051b741a27f940dfbaf93c794e93a55880947064
ZakyMalika <zakymalika0987@gmail.com>
2025-11-07 15:17:31 +0700

    membuat baris Row

--- app/src/main/java/com/example/navigasi_6/View/Form.kt
+++ app/src/main/java/com/example/navigasi_6/View/Form.kt
@@ -53,7 +53,12 @@ fun FormIsian(
                 .padding(20.dp)
                 .width(250.dp), thickness = Thickness, color = Color.Red)
             Row {
+                jenisK.forEach {
+                    item ->
+                    Row(verticalAlignment = Alignment.CenterVertically) {
 
+                    }
+                }
             }
         }
 
