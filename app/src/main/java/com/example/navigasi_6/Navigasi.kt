d216ae5f9c83c4f8b700b0fdd8ae6f6e00a723be
ZakyMalika <zakymalika0987@gmail.com>
2025-11-07 15:46:33 +0700

    membuat function Tampildata

--- app/src/main/java/com/example/navigasi_6/Navigasi.kt
+++ app/src/main/java/com/example/navigasi_6/Navigasi.kt
@@ -1,1 +1,16 @@
 package com.example.navigasi_6
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import java.lang.reflect.Modifier
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TampilData(
+    onBackBtnClick:()-> Unit
+){
+    val items = listOf(
+        Pair
+    )
+}
