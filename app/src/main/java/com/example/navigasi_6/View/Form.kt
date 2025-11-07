77036ac88bfa14dd10b3a883efb328674e856fd5
ZakyMalika <zakymalika0987@gmail.com>
2025-11-07 15:06:41 +0700

    membuat TopAppBar

--- app/src/main/java/com/example/navigasi_6/View/Form.kt
+++ app/src/main/java/com/example/navigasi_6/View/Form.kt
@@ -2,14 +2,30 @@ package com.example.navigasi_6.View
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import com.example.navigasi_6.R
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FormIsian(
-    jenisK:List<String> = Listof("Laki-laki","Perempuan"),
+    jenisK:List<String> = listOf("Laki-laki","Perempuan"),
     OnSubmitBtnClick:()-> Unit
 ){
-    Scaffold(modifier = Modifier) {  }
+    Scaffold(modifier = Modifier),
+            {
+        TopAppBar(
+            title = { Text(stringResource(id = R.string.home), color = Color.White) },
+            colors = TopAppBarDefaults.mediumTopAppBarColors(
+                colorResource(id = R.color.teal_200))
+        )
+    }
+
 }
